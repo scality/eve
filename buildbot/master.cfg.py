ecebf0e50c843e2a9541fb48ac525734d770c13b
Rayene Ben Rayana <rayene.benrayana@scality.com>
2016-06-19 23:20:20 +0200

    add end2end testing logic - happy path

--- buildbot/master.cfg.py
+++ buildbot/master.cfg.py
@@ -1,32 +1,107 @@
-from buildbot.worker import Worker
+import os
+
+import yaml
+import buildbot.plugins
+from buildbot.plugins import util
+from buildbot.process.buildstep import BuildStep
 from buildbot.scheduler import AnyBranchScheduler
 from buildbot.schedulers.forcesched import ForceScheduler
-from buildbot.plugins import changes  # FIXME
-from buildbot.plugins import util  # FIXME
-from buildbot.plugins import steps  # FIXME
+from buildbot.schedulers.triggerable import Triggerable
+from buildbot.steps.shell import SetPropertyFromCommand
+from buildbot.steps.trigger import Trigger
+from buildbot.worker.local import LocalWorker
+from twisted.python.reflect import namedModule
 
-# This is a sample buildmaster config file. It must be installed as
-# 'master.cfg' in your buildmaster's base directory.
+# GIT REPO
+GIT_REPO = os.environ['GIT_REPO']
+assert GIT_REPO
+
+
+# CODE
+
+class ReadConfFromYaml(SetPropertyFromCommand):
+    def __init__(self, **kwargs):
+        SetPropertyFromCommand.__init__(
+            self,
+            name='add steps from yaml',
+            command='cat .buildbot/main.yaml',
+            property='conf',
+            **kwargs)
+
+    def commandComplete(self, cmd):
+        if cmd.didFail():
+            return
+        result = self.observer.getStdout()
+        yresult = yaml.load(result)
+        self.setProperty(self.property, yresult, "SetPropertyFromCommand Step")
+
+
+class StepExtractor(BuildStep):
+    name = 'step extractor'
+
+    def run(self):
+        stage_name = self.getProperty('stage_name', 'main')
+        conf = self.getProperty('conf')
+        assert conf is not None
+        stage_conf = conf['stages'][stage_name]
+        steps = stage_conf['steps']
+        for step in reversed(steps):
+            assert len(step) == 1
+            step_type, params = dict.popitem(step)
+            try:
+                _cls = globals()[step_type]
+            except:
+                _cls = getattr(buildbot.steps.shell, step_type)
+            step = _cls(**params)
+            self.build.addStepsAfterCurrentStep([step])
+        return util.SUCCESS
+
+
+class TriggerStages(Trigger):
+    """ This is a step that allows to start with the properties specified
+        in the schedulerNames argument (tuple) instead of using the properties
+        given in the set_properties/copy_properties parameters.
+
+        This allows to give specific parameter to every scheduler.
+    """
+
+    def __init__(self, stage_names, **kwargs):
+        # self.stage_names = stage_names
+        Trigger.__init__(self, schedulerNames=stage_names, **kwargs)
+
+    def getSchedulersAndProperties(self):
+        conf = self.getProperty('conf')
+        assert conf
+        return [
+            ('s-triggerable', {
+                'stage_name': stage_name,
+                'conf': conf
+            }) for stage_name in self.schedulerNames]
+
+
+# BUILDBOT CONFIGURATION
 
 # This is the dictionary that the buildmaster pays attention to. We also use
 # a shorter alias to save typing.
 c = BuildmasterConfig = {}
 
-
 # PROJECT IDENTITY
 c['title'] = "Eve"
 c['titleURL'] = "http://www.scality.com"
+c['buildbotURL'] = "http://localhost:8020/"
 
-# GIT REPOSITORIES
-GIT_REPOSITORIES = [
-    'https://bitbucket.org/scality/test_buildbot',
-]
 # BUILDWORKERS
-
 # The 'workers' list defines the set of recognized buildworkers. Each element
 # is a Buildworker object, specifying a unique worker name and password.
 # The same worker name and password must be configured on the worker.
-c['workers'] = [Worker("example-worker", "pass")]
+c['workers'] = [
+    LocalWorker('local-worker1'),
+    LocalWorker('local-worker2'),
+]
+
+# The following lines are a workaround for a bug
+for w in c['workers']:
+    w.path_module = namedModule("posixpath")
 
 # 'protocols' contains information about protocols which master will use for
 # communicating with workers. You must define at least 'port' option that
@@ -41,7 +116,7 @@ c['protocols'] = {'pb': {'port': 9989}}
 # about source code changes.  Here we point to the buildbot clone of pyflakes.
 
 c['change_source'] = []
-c['change_source'].append(changes.GitPoller(
+c['change_source'].append(buildbot.plugins.changes.GitPoller(
     'git://github.com/buildbot/pyflakes.git',
     workdir='gitpoller-workdir', branch='master',
     pollinterval=300))
@@ -59,8 +134,11 @@ c['schedulers'].append(AnyBranchScheduler(
 
 c['schedulers'].append(ForceScheduler(
     name="force",
-    builderNames=["b-bootstrap"],
-))
+    builderNames=["b-bootstrap"]))
+
+c['schedulers'].append(Triggerable(
+    name="s-triggerable",
+    builderNames=["b-triggerable"]))
 
 # BUILDERS
 
@@ -69,19 +147,36 @@ c['schedulers'].append(ForceScheduler(
 # what steps, and which workers can execute them.  Note that any particular
 # build will only take place on one worker.
 
-factory = util.BuildFactory()
+bootstrap_factory = util.BuildFactory()
 # check out the source
-factory.addStep(steps.Git(repourl='git://github.com/buildbot/pyflakes.git',
-                          mode='incremental'))
-# run the tests (note that this will require that 'trial' is installed)
-factory.addStep(steps.ShellCommand(command=["trial", "pyflakes"]))
+bootstrap_factory.addStep(
+    buildbot.plugins.steps.Git(repourl=GIT_REPO, mode='incremental'),
+)
+bootstrap_factory.addStep(ReadConfFromYaml())
+bootstrap_factory.addStep(StepExtractor())
+
+# bootstrap_factory.addStep(YamlStepGenerator(
+#    name="parse main.yaml",
+#    command=["cat", ".buildbot/main.yaml"],
+#    haltOnFailure=True)
+# )
+
+triggered_factory = util.BuildFactory()
+triggered_factory.addStep(StepExtractor())
 
 c['builders'] = []
 c['builders'].append(
     util.BuilderConfig(name="b-bootstrap",
-                       workernames=["example-worker"],
-                       factory=factory))
+                       workernames=['local-worker1', 'local-worker2'],
+                       factory=bootstrap_factory))
 
+c['builders'].append(
+    util.BuilderConfig(
+        name="b-triggerable",
+        workernames=['local-worker1', 'local-worker2'],
+        # nextSlave=latent_worker_chooser,
+        factory=triggered_factory
+    ))
 # STATUS TARGETS
 
 # 'status' is a list of Status Targets. The results of each build will be
@@ -89,13 +184,6 @@ c['builders'].append(
 # like IRC bots.
 
 c['status'] = []
-
-# the 'buildbotURL' string should point to the location where the buildbot's
-# internal web server is visible. This typically uses the port number set in
-# the 'www' entry below, but with an externally-visible host name which the
-# buildbot cannot figure out without some help.
-
-c['buildbotURL'] = "http://localhost:8020/"
 
 # minimalistic config to activate new web UI
 c['www'] = dict(port=8020,
@@ -108,3 +196,9 @@ c['db'] = {
     # You can leave this at its default for all but the largest installations.
     'db_url': "sqlite:///state.sqlite",
 }
+
+
+@util.renderer
+def get_bitbucket_url_with_credentials(props):
+    repo_url = props.getProperty('repository')
+    return repo_url.replace('https://bitbucket.org/', 'git@bitbucket.org:')
--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -1,30 +1,117 @@
-import unittest
+import os
 import subprocess
+import time
+import unittest
 from subprocess import check_output as cmd
+
+import requests
 
 
 def run_buildbot():
     try:
-        cmd(['buildbot', 'stop', 'master'])
+        cmd(['buildbot', 'stop', '/tmp/buildbot-master'])
+        cmd(['rm', '-rf', '/tmp/buildbot-master'])
     except subprocess.CalledProcessError:
         pass
-    cmd(['buildbot', 'create-master', 'master'])
-    cmd(['cp', 'buildbot/master.cfg.py', 'master/master.cfg'])
-    # cmd(['cp', 'master/master.cfg.sample', 'master/master.cfg'])
-    execfile('master/master.cfg')
+    cmd(['cp', '-r', 'buildbot', '/tmp/buildbot-master'])
+    cmd(['mv', '/tmp/buildbot-master/master.cfg.py',
+         '/tmp/buildbot-master/master.cfg'])
+    cmd(['buildbot', 'create-master', '/tmp/buildbot-master'])
+
+    cfg_file = '/tmp/buildbot-master/master.cfg'
+    with open(cfg_file, "rb") as f:
+        compile(f.read(), cfg_file, 'exec')
+
     try:
-        cmd(['buildbot', 'start', 'master'])
+        cmd(['buildbot', 'start', '/tmp/buildbot-master'])
         return True
     except subprocess.CalledProcessError as e:
-        with open('master/twistd.log') as logfile:
+        with open('/tmp/buildbot-master/twistd.log') as logfile:
             print(logfile.read())
         print "Ping stdout output:\n", e.output
         raise
 
 
+class BuildbotDataAPi():
+    def __init__(self):
+        self.base_url = 'http://localhost:8020/api/v2/'
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json, text/plain, */*',
+        }
+
+    def post(self, route, method, params={}):
+        data = {
+            'id': 1,
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params
+        }
+        res = requests.post(self.base_url + route, json=data,
+                            headers=self.headers)
+        print res.json()
+        res.raise_for_status()
+        return res.json()
+
+    def get(self, route):
+        res = requests.get(self.base_url + route, headers=self.headers)
+        res.raise_for_status()
+        return res.json()
+
+    def get_element_id_from_name(self, route, name, id_key, name_key='name'):
+        elements = self.get(route)[route]
+        for e in elements:
+            if e[name_key] == name:
+                _id = e[id_key]
+                break
+        else:
+            raise Exception('element not found')
+        return _id
+
+    def force_build(self, builderid):
+        params = {
+            'builderid': str(builderid),
+            'username': '',
+            'reason': 'force build',
+            'repository': '',
+            'project': '',
+            'branch': '',
+            'revision': ''
+        }
+        print params
+        self.post('forceschedulers/force', 'force', params=params)
+
+
 class TestEnd2End(unittest.TestCase):
     def setUp(self):
+        import tempfile
+        d = tempfile.mkdtemp()
+        cmd(['mkdir', d + '/.buildbot'])
+        cmd(['cp', 'tests/success.yml', d + '/.buildbot/main.yaml'])
+        old_dir = os.getcwd()
+        os.chdir(d)
+        cmd(['git', 'init'])
+        cmd(['git', 'config', 'user.email', 'john@example.com'])
+        cmd(['git', 'config', 'user.name', 'john'])
+        cmd(['git', 'add', '.buildbot/main.yaml'])
+        cmd(['git', 'commit', '-m', 'first commit'])
+        os.environ['GIT_REPO'] = d
+        os.chdir(old_dir)
         run_buildbot()
+        self.api = BuildbotDataAPi()
 
     def test_run(self):
-        pass
+        bootstrap_builder_id = self.api.get_element_id_from_name(
+            'builders',
+            'b-bootstrap',
+            'builderid')
+        self.api.force_build(bootstrap_builder_id)
+        for i in range(120):
+            time.sleep(1)
+            build = self.api.get('builds/1')['builds'][0]
+            state = build['state_string']
+            print state
+            if state != 'starting':
+                break
+        assert state == 'finished'
+        assert build['results'] == 0  # SUCCESS
