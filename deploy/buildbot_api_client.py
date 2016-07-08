2052bb774554ea1624ae8109903e3723df6da64e
Akim Sadaoui <akim.sadaoui@scality.com>
2016-07-08 16:18:44 +0200

    Add HTTP basic auth support to BuildbotDataAPI

--- deploy/buildbot_api_client.py
+++ deploy/buildbot_api_client.py
@@ -8,6 +8,7 @@ for more info on the API, see `buildbot REST API documentation`_
 """
 
 import requests
+from requests.auth import HTTPBasicAuth
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # Hack to remove a lot of warnings in stdout while testing
@@ -23,6 +24,11 @@ class BuildbotDataAPI(object):
             'Content-Type': 'application/json',
             'Accept': 'application/json, text/plain, */*',
         }
+        self.auth = None
+
+    def add_auth(self, user, password):
+        """Use given `user` and `password` for HTTP basic auth."""
+        self.auth = HTTPBasicAuth(user, password)
 
     def post(self, route, method, params=None):
         """Post data to the REST API."""
@@ -34,13 +40,14 @@ class BuildbotDataAPI(object):
         }
 
         res = requests.post(self.base_url + route, json=data,
-                            headers=self.headers)
+                            headers=self.headers, auth=self.auth)
         res.raise_for_status()
         return res.json()
 
     def get(self, route):
         """Get data from the REST API."""
-        res = requests.get(self.base_url + route, headers=self.headers)
+        res = requests.get(self.base_url + route, headers=self.headers,
+                           auth=self.auth)
         res.raise_for_status()
         return res.json()
 
