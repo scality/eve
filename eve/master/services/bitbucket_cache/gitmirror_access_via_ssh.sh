24c21957b5824fa0c286ba35d168463ffd5704c0
Rayene Ben Rayana <rayene.benrayana@scality.com>
2017-01-10 13:53:30 +0100

    fix bitbucket_cache script

--- eve/master/services/bitbucket_cache/gitmirror_access_via_ssh.sh
+++ eve/master/services/bitbucket_cache/gitmirror_access_via_ssh.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+set -e
 
+LOG_FILE=~/bitbucket_cache.log
 # ensure the command is allowed
-ACCEPTED_PATTERNS="git-upload-pack '[a-zA-Z_-]+/[a-zA-Z_-]+\.git'"
+ACCEPTED_PATTERNS="git-[/a-zA-Z]+-pack '[/a-zA-Z_-]+/[a-zA-Z_-]+\.git'"
 if ! $(echo "$SSH_ORIGINAL_COMMAND" | grep -qE "$ACCEPTED_PATTERNS")
 then
-    echo "only git-upload-pack commands are allowed. Received '$SSH_ORIGINAL_COMMAND'"
+    echo "only git-*-pack commands are allowed. Received '$SSH_ORIGINAL_COMMAND'" | tee -a ${LOG_FILE}
     exit 1
 fi
 
 # clone the repo if not done yet
-
-repo=$(echo "$SSH_ORIGINAL_COMMAND" | sed -re "s/^.*'[a-zA-Z_-]+\/([a-zA-Z_-]+)\.git'/\1/")
-repo_dest=/home/git/scality/$repo.git
-if [ ! -d "$repo_dest" ]
-then
-    git clone --mirror git@bitbucket.org:scality/$repo $repo_dest
-fi
-
-cd $repo_dest;
-if [ ! "$repo" = 'mock' ];
-then
-    git remote update;
-fi
+(
+    repo=$(echo "$SSH_ORIGINAL_COMMAND" | sed -re "s/^.*'[/a-zA-Z_-]+\/([a-zA-Z_-]+)\.git'/\1/")
+    repo_dest=/home/git/scality/$repo.git
+    if [ -d "$repo_dest" ]
+    then
+        cd $repo_dest;
+        git remote update || exit 1;
+    else
+        mkdir -p /home/git/scality
+        git clone --mirror git@bitbucket.org:scality/$repo.git $repo_dest || exit 1
+    fi
+) >> ${LOG_FILE} 2>&1 || exit 1
 
 
 # execute git-upload-pack
