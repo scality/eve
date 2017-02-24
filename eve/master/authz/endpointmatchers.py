c2231b88655478e3d40d189845e07dd47b3f3fd8
Jean-Baptiste Bronisz <jean-baptiste.bronisz@scality.com>
2017-02-24 14:49:55 +0100

    EVE-338 - Change the rebuild behaviour
    We ignore now all intermediates build of bootstrap and allow the others.

--- eve/master/authz/endpointmatchers.py
+++ eve/master/authz/endpointmatchers.py
@@ -3,8 +3,8 @@ import buildbot.www.authz.endpointmatchers as bb_endpointmatchers
 from twisted.internet import defer
 
 
-class DenyRebuildBuildEndpointMatcher(bb_endpointmatchers.EndpointMatcherBase):
-    """Build endpoint matcher class to deny rebuild.
+class DenyRebuildIntermediateBuild(bb_endpointmatchers.EndpointMatcherBase):
+    """Build endpoint matcher class to deny rebuild on intermediate build.
 
     This class differs from `~.endpointmatchers.RebuildBuildEndpointMatcher`.
     This latter class is incomplete and doesn't match any builder.
@@ -14,17 +14,15 @@ class DenyRebuildBuildEndpointMatcher(bb_endpointmatchers.EndpointMatcherBase):
     It is the responsibility of :meth:`~.authz.Autz.assertUserAllowed`
     to deny access if the role doesn't match.
 
-    Here we need to deny the endpoint for all roles and allow it only
-    for the given builders.
+    Here we need to deny the endpoint for all roles if the build is an
+    intermediate build of the given builder.
     """
-    def __init__(self, allow_builders=None, **kwargs):
+    def __init__(self, root_builder_name, **kwargs):
         """`DenyRebuildBuildEndpointMatcher` constructor.
 
-        :args allow_builder: List of allowed builders name
-
-        All the other builders will be denied.
+        :args root_builder_name: Name of the root builder.
         """
-        self.allow_builders = allow_builders or []
+        self.root_builder_name = root_builder_name
 
         bb_endpointmatchers.EndpointMatcherBase.__init__(self, **kwargs)
 
@@ -32,25 +30,39 @@ class DenyRebuildBuildEndpointMatcher(bb_endpointmatchers.EndpointMatcherBase):
     def match_BuildEndpoint_rebuild(self, epobject, epdict, _):
         """Called by `~.EndpointMatcherBase.match` for rebuild endpoint.
 
-        :raises Forbidden: If the builder name doesn't match
-                           ``allow_builders``.
+        :raises Forbidden: If the builder name is an intermediate
+                             build of the given root builder.
         """
         build = yield epobject.get({}, epdict)
+        buildrequest = yield self.master.data.get(
+            ('buildrequests', build['buildrequestid'])
+        )
+        buildset = yield self.master.data.get(
+            ('buildsets', buildrequest['buildsetid'])
+        )
+
+        if buildset['parent_buildid'] is None:
+            defer.returnValue(None)
+
+        while buildset['parent_buildid'] is not None:
+            build = yield self.master.data.get(
+                ('builds', buildset['parent_buildid'])
+            )
+            buildrequest = yield self.master.data.get(
+                ('buildrequests', build['buildrequestid'])
+            )
+            buildset = yield self.master.data.get(
+                ('buildsets', buildrequest['buildsetid'])
+            )
+
         builder = yield self.master.data.get(
             ('builders', build['builderid'])
         )
 
-        for allow_builder in self.allow_builders:
-            if self.authz.match(builder['name'], allow_builder):
-                break
-        else:
+        if self.authz.match(builder['name'], self.root_builder_name):
             raise bb_authz.Forbidden(
-                "This builder is not allowed to be rebuilt."
-                " Please select the 'bootstrap' builder."
+                'This builder is not allowed to be rebuilt.'
+                ' Please select the "{0}" builder.'.format(
+                    builder["name"]
+                )
             )
-
-        if self.role:
-            match = bb_endpointmatchers.Match(self.master, build=build)
-        else:
-            match = None
-        defer.returnValue(match)
