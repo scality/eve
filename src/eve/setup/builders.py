96eba48c9d5c6116c8128a4011f8c9f1302d69fa
Sylvain Killian <sylvain.killian@scality.com>
2017-03-28 17:45:10 +0200

    EVE-361 configure git cache via an extra step for docker workers

--- src/eve/setup/builders.py
+++ src/eve/setup/builders.py
@@ -1,12 +1,30 @@
 from buildbot.config import BuilderConfig
-from buildbot.plugins import steps
+from buildbot.plugins import steps, util
 from buildbot.process.factory import BuildFactory
 from buildbot.process.results import SUCCESS
+from buildbot.steps.shell import ShellCommand
 
 
 def triggerable_builder(builder_name, workers):
     factory = BuildFactory()
     factory.addStep(steps.CancelOldBuild())
+
+    # customize global Git conf to hit on docker cache
+    if builder_name == util.env.DOCKER_BUILDER_NAME:
+        factory.addStep(ShellCommand(
+            name='customize git settings to hit on cache',
+            hideStepIf=lambda results, s: results == SUCCESS,
+            haltOnFailure=True,
+            command='git config --global '
+                    'url.http://git_cache/bitbucket.org/.insteadOf '
+                    'git@bitbucket.org: && '
+                    'git config --global '
+                    'url.http://git_cache/github.com/.insteadOf '
+                    'git@github.com: && '
+                    'git config --global '
+                    'url.http://git_cache/mock/.insteadOf git@mock:'
+        ))
+
     # Extract steps from conf
     factory.addStep(steps.StepExtractor(
         name='extract steps from yaml',
