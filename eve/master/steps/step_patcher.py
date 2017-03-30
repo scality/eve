10502928b31a16b0fa95cef02d8e4cb3373206c9
Arnaud Calmettes <calmettes.arnaud@gmail.com>
2017-03-30 15:21:55 +0200

    EVE-390 Add StepPatcher feature
    This feature adds a step that loads a yaml patch file located at
    `PATCHER_FILE_PATH` and sets a `step_patcher_config` build property with its
    contents. If the file is absent, the feature is disabled.
    
    During the Step extraction, feed a StepPatcher with the config read earlier and
    use it to patch the arguments given to the step creation.
    
    Skipping steps is the only currently supported feature:
    
        skip_steps:
            - .*PLIP   # Skip all tests containing 'PLIP'
            - plop    # Skip all tests starting with 'plop'
    
    A single string can be passed to this mapping
    
        skip_tests: ".*PLIP"

---
+++ eve/master/steps/step_patcher.py
@@ -0,0 +1,91 @@
+"""Step patching feature."""
+
+import re
+import yaml
+
+from buildbot.process.buildstep import BuildStep
+from buildbot.process.results import (
+    ERROR, SUCCESS, SKIPPED, WARNING
+)
+
+from twisted.internet import defer
+from twisted.logger import Logger
+
+
+class StepPatcherConfig(BuildStep):
+    """Parses the patch config file and sets the corresponding property.
+
+    The config file path should be set in the PATCHER_FILE_PATH environment
+    variable. If this variable is not set, the step patching feature is
+    disabled.
+
+    """
+    name = 'step patcher config'
+    logger = Logger('eve.steps.StepPatcherReader')
+
+    def __init__(self, conf_path=None, **kwargs):
+        kwargs.setdefault('haltOnFailure', False)
+        super(StepPatcherConfig, self).__init__(**kwargs)
+        self.conf_path = conf_path
+
+    def run(self):
+        prop = 'step_patcher_config'
+        if not self.conf_path:
+            self.logger.debug('PATCHER_FILE_PATH is not set. Skipping')
+            self.setProperty(prop, {}, 'StepPatcherConfig')
+            return defer.succeed(SKIPPED)
+
+        config = {}
+        try:
+            with open(self.conf_path) as patch_file:
+                config = yaml.load(patch_file.read())
+        except Exception as err:
+            self.logger.error(
+                'An error occured while loading the patcher config file at '
+                '{path}: {err}', path=self.conf_path, err=err)
+            self.setProperty(prop, {}, 'StepPatcherConfig')
+            return defer.succeed(ERROR)
+
+        self.setProperty('step_patcher_config', config, 'StepPatcherConfig')
+        if not config:
+            return defer.succeed(SUCCESS)
+        else:
+            self.logger.info("Setting patching config: {conf}",
+                             conf=config)
+            defer.succeed(WARNING)
+
+
+class StepPatcher(object):
+    """Generic hook to patch step types and parameters."""
+
+    logger = Logger('eve.steps.StepPatcher')
+
+    def __init__(self, config={}):
+        skip_tests = config.get('skip_tests', [])
+        self.skip_regexp = None
+        if isinstance(skip_tests, basestring):
+            try:
+                self.skip_regexp = re.compile(skip_tests)
+            except re.error as err:
+                self.logger.error(
+                    "Couldn't compile a regexp from '{regexp}': {err}",
+                    regexp=skip_tests, err=err
+                )
+        elif isinstance(skip_tests, (list, tuple)):
+            try:
+                self.skip_regexp = re.compile('|'.join(skip_tests))
+            except (TypeError, re.error) as err:
+                self.logger.error(
+                    "Couldn't compile a master regexp from '{regexp}': {err}",
+                    regexp=skip_tests, err=err
+                )
+
+    def patch(self, step_type, params):
+        if not self.skip_regexp:
+            return step_type, params
+
+        if self.skip_regexp.match(params.get('name', '')):
+            params['doStepIf'] = False
+            params['descriptionDone'] = 'Temporarily disabled'
+
+        return step_type, params
