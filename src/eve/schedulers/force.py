ed8444ee17c773bdf614ae361da2c72147732492
bertrand.demiddelaer <bertrand.demiddelaer@scality.com>
2017-04-12 17:44:29 +0200

    use collectValidationErrors

--- src/eve/schedulers/force.py
+++ src/eve/schedulers/force.py
@@ -1,6 +1,6 @@
 import subprocess
 
-from buildbot.schedulers.forcesched import ForceScheduler
+from buildbot.schedulers.forcesched import ForceScheduler, ValidationError
 from twisted.internet import defer, threads
 
 
@@ -13,20 +13,26 @@ class EveForceScheduler(ForceScheduler):
                 collector, **kwargs
             )
 
-        yield threads.deferToThread(self.add_missing_revisions, sourcestamps)
+        yield collector.collectValidationErrors('branch',
+                                                self.add_missing_revisions,
+                                                sourcestamps)
 
         defer.returnValue((properties, changeids, sourcestamps))
 
     @staticmethod
+    @defer.inlineCallbacks
     def add_missing_revisions(sourcestamps):
         for _, sourcestamp in sourcestamps.iteritems():
             if not sourcestamp['revision']:
+                # Retrieve revision from branch for sourcestamps without one
+                res = yield threads.deferToThread(
+                    subprocess.check_output,
+                    ['git', 'ls-remote', sourcestamp['repository'],
+                     sourcestamp['branch']],
+                    stderr=subprocess.PIPE,
+                )
                 try:
-                    # Retrieve revision from branch for sourcestamps without one
-                    sourcestamp['revision'] = subprocess.check_output(
-                        ['git', 'ls-remote', sourcestamp['repository'],
-                         sourcestamp['branch']],
-                        stderr=subprocess.PIPE,
-                    ).split()[0]
+                    sourcestamp['revision'] = res.split()[0]
                 except IndexError:
-                    sourcestamp['revision'] = None
+                    raise ValidationError("'%s' branch not found" %
+                                          sourcestamp['branch'])
