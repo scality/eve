611f89e8e1e0e9a32f3cbfba64309d98f9fa5fc9
Rayene Ben Rayana <rayene.benrayana@scality.com>
2017-04-19 14:20:49 +0200

    improve git-proxy cache

--- eve/services/git_cache/git-proxy-cache.py
+++ eve/services/git_cache/git-proxy-cache.py
@@ -7,11 +7,15 @@ project_root = os.environ['GIT_PROJECT_ROOT']
 reporelpath = os.environ['PATH_INFO']
 # e.g. reporelpath = /github.com/organization/myrepo.git/info/refs
 
-_, githost, account, reponame, _ = reporelpath.split('/', 4)
-repocachedir = os.path.join(project_root, githost, account, reponame)
+_, scheme, githost, account, reponame, _ = reporelpath.split('/', 5)
+repocachedir = os.path.join(project_root, scheme, githost, account, reponame)
 
 # if a cached repository already exists, we just refresh it.
 if os.path.exists(repocachedir):
+    try:
+        os.makedirs(repocachedir)
+    except OSError:
+        pass
     os.chdir(repocachedir)
     subprocess.check_output(['git',
                              'remote',
@@ -25,26 +29,11 @@ else:
     except OSError:
         pass
 
-    if githost == 'mock':
-        # for testing purposes, we can use mock as an organization. In this
-        # case, the repo is dynamically created locally with write access.
-        subprocess.check_output(['git',
-                                 'init',
-                                 '--bare',
-                                 '--quiet',
-                                 repocachedir])
-        os.chdir(repocachedir)
-        subprocess.check_output(['git',
-                                 'config',
-                                 'http.receivepack',
-                                 'true'])
+    remote_url = '{}://{}/{}/{}'.format(scheme, githost, account, reponame)
 
-    else:
-        # We need to clone a remote repo to create the cache.
-        remote_url = 'https://{}/{}/{}'.format(githost, account, reponame)
-        subprocess.check_output(['git',
-                                 'clone',
-                                 '--quiet',
-                                 '--mirror',
-                                 remote_url,
-                                 repocachedir])
+    subprocess.check_output(['git',
+                             'clone',
+                             '--quiet',
+                             '--mirror',
+                             remote_url,
+                             repocachedir])
