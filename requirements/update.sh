2e8cbb09c7af091fa3dee3e643871f93b7be8943
Rayene Ben Rayana <rayene.benrayana@scality.com>
2017-04-19 13:43:47 +0200

    update requirements

---
+++ requirements/update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+for i in autoformat base compose flake8 prospector pylint system_tests unit; do
+    echo item: $i
+    freeze-requirements freeze -m "${1:-.}"/$i.txt "${1:-.}"/$i.in
+done
