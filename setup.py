2005f32b641fcad36cc622e80072c14dd3ab87ca
Sylvain Killian <sylvain.killian@scality.com>
2017-04-26 18:25:22 +0200

    EVE-442 another cleanup pass on requirements, setup and tox

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ CWD = dirname(abspath(__file__))
 
 
 def requires():
-    # FIXME: putting 'requirements/base.in' below does not work!
-    reqs_file = join(CWD, 'requirements/base.txt')
+    reqs_file = join(CWD, 'requirements/base.in')
     reqs_install = parse_requirements(reqs_file, session=PipSession())
 
     return [str(ir.req) for ir in reqs_install]
