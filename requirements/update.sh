a1e4db40e1fa5746191c167cefae225bce5cbcf5
Rayene Ben Rayana <rayene.benrayana@scality.com>
2017-05-03 17:34:57 +0200

    add pydocstyle blocking step

--- requirements/update.sh
+++ requirements/update.sh
@@ -19,7 +19,7 @@
 # Boston, MA  02110-1301, USA.
 
 set -e
-for i in autoformat base compose flake8 freeze pylint system_tests unit; do
+for i in autoformat base compose flake8 freeze pydocstyle pylint system_tests unit; do
     echo item: $i
     freeze-requirements freeze -m "${1:-.}"/$i.txt "${1:-.}"/$i.in
 done
