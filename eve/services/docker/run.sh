130917f1a39d4509551d6c9ae95091a41d404ce3
Sylvain Killian <sylvain.killian@scality.com>
2017-07-18 11:07:11 +0200

    EVE-547 refresh docker login token regularly

--- eve/services/docker/run.sh
+++ eve/services/docker/run.sh
@@ -1,9 +1,20 @@
 #!bin/bash
 
-docker login -u oauth2accesstoken -p "$(gcloud auth application-default print-access-token)" https://gcr.io
+while true; do
+    RES=$(docker login \
+            -u oauth2accesstoken \
+            -p "$(gcloud auth application-default print-access-token)" \
+            https://gcr.io 2>&1)
+    echo "$(date) ${RES}" >> /var/log/docker_login.log
+    sleep 2400
+done &
 
 redis-server > /var/log/redis.log &
+
 for i in $(seq 3); do
-    celery -A app.celery worker --concurrency=10 -n worker$i@%h > /var/log/celery$i.log &
+    celery -A app.celery worker \
+           --concurrency=10 \
+           -n worker$i@%h > /var/log/celery$i.log &
 done
+
 flask run --host=0.0.0.0
