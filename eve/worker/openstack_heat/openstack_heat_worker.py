c48c9e1710874a60429eff101734518882c32c35
bertrand.demiddelaer <bertrand.demiddelaer@scality.com>
2017-08-01 12:12:11 +0200

    preparing worker will not wait 1h to fail anymore.

--- eve/worker/openstack_heat/openstack_heat_worker.py
+++ eve/worker/openstack_heat/openstack_heat_worker.py
@@ -29,6 +29,9 @@ from twisted.internet import defer, threads
 from twisted.logger import Logger
 
 
+MISSING_TIMEOUT = 15 * 60
+
+
 class HeatLatentWorker(AbstractLatentWorker):
     """Buildbot worker using Openstack heat."""
 
@@ -67,6 +70,11 @@ class HeatLatentWorker(AbstractLatentWorker):
         sess = session.Session(auth=auth)
         self.heat_client = heatclient.client.Client(
             '1', session=sess, region_name=os_region_name)
+
+    def reconfigService(self, name, password,
+                        **kwargs):
+        kwargs.setdefault('missing_timeout', MISSING_TIMEOUT)
+        super(HeatLatentWorker, self).reconfigService(name, password, **kwargs)
 
     @defer.inlineCallbacks
     def start_instance(self, build):
