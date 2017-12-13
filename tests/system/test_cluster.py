a01143c86154b84e9e8d33002130f907e87b6366
bertrand.demiddelaer <bertrand.demiddelaer@scality.com>
2017-12-13 11:03:17 +0100

    remove pdb call

--- tests/system/test_cluster.py
+++ tests/system/test_cluster.py
@@ -65,7 +65,6 @@ class TestCluster(unittest.TestCase):
         with Cluster() as cluster:
             local_repo = cluster.clone()
             local_repo.push()
-            import pdb; pdb.set_trace()
             cluster.api.force(branch=local_repo.branch)
 
             # Check bootstrap
