1ba0ffa6f550abf81a76a056ad5ea634f4087072
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2018-07-13 13:54:03 +0200

    fix

---
+++ check_multiple_big_downloads.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir download
+cd download
+
+for i in `seq 0 9`
+do
+    wget -q -O data.$i "%(prop:artifacts_private_url)s/data" &
+done
+wait
+
+for i in `seq 0 9`
+do
+    cat ../big/data.sha1 | sed -e "s/data/data.$i/" >> data.sha1
+done
+
+ls -l ../big/data
+cat ../big/data.sha1
+ls -l data*
+cat data.sha1
+sha1sum -c data.sha1
