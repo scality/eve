e76e50f74b3b34fad572a6e2f0ea59e47a644c3f
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2018-07-13 15:02:32 +0200

    fix

--- check_multiple_big_downloads.sh
+++ check_multiple_big_downloads.sh
@@ -9,30 +9,28 @@ uri=/$(echo $full_url | cut -d/ -f4-)
 mkdir download
 cd download
 
-# try to mess the uwsgi cache with concurrent interrupted GET operations
-
+# try to mess the uwsgi cache with concurrent interrupted GET operations while downloading the data
+wget -q -O data.before $full_url &
 for i in `seq 0 9`
 do
     bytes=$(( 1000000000 + $i * 100000000))
     (/bin/echo -e "GET /$uri HTTP/1.1\nHost: $host\n\n" | nc $host $port | head -c $bytes > /dev/null)&
 done
-
+wget -q -O data.after $full_url &
 wait
 
 # launch concurrent complete downloads
-
 for i in `seq 0 9`
 do
     wget -q -O data.$i $full_url &
 done
-
 wait
 
 # check complete downloads
-
+cat ../big/data.sha1 | sed -e "s/data/data.before/" >> data.sha1
+cat ../big/data.sha1 | sed -e "s/data/data.after/" >> data.sha1
 for i in `seq 0 9`
 do
     cat ../big/data.sha1 | sed -e "s/data/data.$i/" >> data.sha1
 done
-
 sha1sum -c data.sha1
