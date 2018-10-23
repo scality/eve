91efa7b2f117f38b7e9c15d6b5003cb79519b3a3
Sylvain Killian <sylvain.killian@scality.com>
2018-10-23 11:17:57 +0200

    PIPDEP-595 update method to install docker-hook

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -418,6 +418,7 @@ class TestDockerCluster(unittest.TestCase):
         """
         conf = {
             'DOCKER_HOOK_IN_USE': '1',
+            'DOCKER_HOOK_FAKE_INSTALL': '1',
             'DOCKER_HOOK_VERSION': '1.2.3',
             'DOCKER_HOOK_WORKERS': 'riri;ubuntu-xenial-with-docker-ctxt;plop',
         }
@@ -459,6 +460,7 @@ class TestDockerCluster(unittest.TestCase):
         """
         conf = {
             'DOCKER_HOOK_IN_USE': '1',
+            'DOCKER_HOOK_FAKE_INSTALL': '1',
             'DOCKER_HOOK_VERSION': '1.2.3',
             'DOCKER_HOOK_WORKERS': 'ri;not-ubuntu-xenial-with-docker-ctxt;pl',
         }
