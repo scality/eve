0c95f1842030260169e7f0287f1b5ef434aea7d2
Thomas Carmet <thomas.carmet@scality.com>
2019-03-04 15:47:42 +0100

    removing dead code in codecov test

--- tests/system/codecovio/test_codecov.py
+++ tests/system/codecovio/test_codecov.py
@@ -93,29 +93,6 @@ class TestPublishCodeCoverage(unittest.TestCase):
 
         self.cluster.stop()
 
-    def _get_publish_codecov_build(self, builder_name='test_docker_builder'):
-        """Search the build of the code coverage report publication step.
-
-        Args:
-            builder_name: Root builder name rather than bootstrap.
-
-        Returns:
-            None if no potential build else the build found.
-
-        """
-        builders = self.cluster.api.get(
-            'builders?name__contains={0}'.format(builder_name))
-        for builder in builders['builders']:
-            builds_url = 'builders/{0}/builds'.format(builder['builderid'])
-            builds = self.cluster.api.get(builds_url)
-            for build in builds['builds']:
-                steps = self.cluster.api.get(
-                    '{0}/{1}/steps?name__contains=coverage'.format(
-                        builds_url, build['number']))
-                if steps:
-                    return build
-        return None
-
     def test_codecovio_success(self):
         """Test PublishCoverageReport success.
 
