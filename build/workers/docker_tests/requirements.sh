4bb917e3b8b54a808c015fe299701d7af20fb725
Thomas Carmet <thomas.carmet@scality.com>
2019-09-06 14:05:17 +0200

    EVE-1089 update key server

--- build/workers/docker_tests/requirements.sh
+++ build/workers/docker_tests/requirements.sh
@@ -9,7 +9,7 @@ apt-get install -y apt-transport-https
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" \
     > /etc/apt/sources.list.d/docker.list
 apt-key adv \
-    --keyserver hkp://p80.pool.sks-keyservers.net:80 \
+    --keyserver keyserver.ubuntu.com \
     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 # install docker
