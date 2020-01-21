207b04ce0e32955d53ab4e06bacaf859aeb35f19
Bertrand Demiddelaer <bertrand.demiddelaer@scality.com>
2020-01-21 11:51:49 +0100

    EVE-1111 fix \x interpretation by python

--- eve/steps/artifacts.py
+++ eve/steps/artifacts.py
@@ -369,7 +369,7 @@ class Uploadv3(BaseUpload):
     def upload_command(self):
         return [
             ('find -L -type f -print0 | '
-             'sed -e "s:\(^\|\x0\)\./:\1:g" | '
+             'sed -e "s:\\(^\\|\\x0\\)\\./:\\1:g" | '
              'xargs -0 -n 1 -t -P 16 '
              '-I @ curl --silent --fail --show-error --max-time {} '
              '-T "@" "http://artifacts-v3/upload/{}/@"').format(
